70073e503c755a041a7f8b784b67cddf6425c144
ChulaniKarandana <chulani.15@cse.mrt.ac.lk>
2017-06-29 10:15:57 +0530

    Add files via upload

---
+++ hello.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello World <br>";
+
+$foo = 25;
+$zoo = "Chulani Lakmali";
+
+echo "$zoo <br>" ;
+echo $foo .'<br>';
+
+$google = "Google Link" ;
+
+echo '<a href = "https:www.google.com">'.$google.'</a>';
+
+
+
+?>
